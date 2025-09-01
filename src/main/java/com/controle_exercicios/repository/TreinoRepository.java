fd4a3fc35efec2c1f950a3c29b31a67e744a1cc0
Felix Eduardo Villanueva Condori <felixeduardobx@gmail.com>
2025-09-01 10:18:22 -0300

    Services Criados

--- src/main/java/com/controle_exercicios/repository/TreinoRepository.java
+++ src/main/java/com/controle_exercicios/repository/TreinoRepository.java
@@ -1,7 +1,15 @@
 package com.controle_exercicios.repository;
 
 import com.controle_exercicios.entity.Treino;
+import com.controle_exercicios.entity.enums.Intensidade;
+import com.controle_exercicios.entity.enums.Tipo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TreinoRepository extends JpaRepository <Treino, Long>{
+   
+    List<Treino> findByTipo(Tipo tipo);
+
+    List<Treino> findByIntensidade(Intensidade intensidade);
 }
--- src/main/java/com/controle_exercicios/service/TreinoService.java
+++ src/main/java/com/controle_exercicios/service/TreinoService.java
@@ -1,8 +1,18 @@
 package com.controle_exercicios.service;
 
 import com.controle_exercicios.dto.TreinoCreateDTO;
+import com.controle_exercicios.dto.TreinoResponseDTO;
+import com.controle_exercicios.dto.TreinoUpdateDTO;
+import com.controle_exercicios.entity.Treino;
+import com.controle_exercicios.entity.enums.Intensidade;
+import com.controle_exercicios.entity.enums.Tipo;
 import com.controle_exercicios.repository.TreinoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TreinoService {
 
@@ -10,15 +20,52 @@ public class TreinoService {
     private TreinoRepository repository;
 
     //CREATE
-    public criarTreino(TreinoCreateDTO dto){
+    public TreinoResponseDTO criarTreino (TreinoCreateDTO dto){
+        Treino treino = new Treino();
+        treino.setTipo(dto.getTipo());
+        treino.setDuracao(dto.getDuracao());
+        treino.setData(dto.getData());
+        treino.setIntensidade(dto.getIntensidade());
 
+        Treino save = repository.save(treino);
+        return new TreinoResponseDTO(save);
+    }
+    //LIST
+    public List<TreinoResponseDTO> listar(Tipo tipo, Intensidade intensidade){
+        List<Treino> treinos;
+        if (tipo != null){
+            treinos = repository.findByTipo(tipo);
+        } else if (intensidade != null) {
+            treinos = repository.findByIntensidade(intensidade);
+        }else {
+            treinos = repository.findAll();
+        }
+        return treinos.stream().map(TreinoResponseDTO::new).collect(Collectors.toList());
+    }
+    //FIND BY ID
+    public TreinoResponseDTO BuscarPorID(Long id){
+        Treino treino = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Treino não encontrado"));
+        return new TreinoResponseDTO(treino);
+    }
+    //UPDATE
+    public TreinoResponseDTO atualizarTreino(TreinoUpdateDTO dto, Long id){
+        Treino treino = repository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException("Treino não encontrado"));
+        if (dto.getTipo() != null) treino.setTipo(dto.getTipo());
+        if (dto.getDuracao() != null) treino.setDuracao(dto.getDuracao());
+        if (dto.getData() != null) treino.setData(dto.getData());
+        if (dto.getIntensidade() != null) treino.setIntensidade(dto.getIntensidade());
+
+        Treino atualizado = repository.save(treino);
+        return new TreinoResponseDTO(atualizado);
     }
 
-    //LIST
-
-
-    //UPDATE
-
-
     //DELETE
+    public void deletarTreino(Long id){
+        if(!repository.existsById(id)){
+            throw new EntityNotFoundException("Treino não encontrado");
+        }
+        repository.deleteById(id);
+    }
 }
